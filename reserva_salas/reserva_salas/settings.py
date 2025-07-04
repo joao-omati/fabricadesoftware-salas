cb49ea374ce3f15e88133a62a9d14117c9613f7b
Paulo V. <paulocruvel@gmail.com>
2025-07-04 16:18:08 -0300

    criação da base.html

--- reserva_salas/reserva_salas/settings.py
+++ reserva_salas/reserva_salas/settings.py
@@ -68,7 +68,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
