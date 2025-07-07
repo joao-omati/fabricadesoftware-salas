5ca038f0ad9900113528470253a69506a1d2671a
Paulo V. <paulocruvel@gmail.com>
2025-07-07 13:49:38 -0300

    criação da conexão com o BD

--- reserva_salas/reserva_salas/settings.py
+++ reserva_salas/reserva_salas/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from . import conection
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,8 +90,12 @@ WSGI_APPLICATION = 'reserva_salas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': conection.DATA_BASE,
+        'USER': 'postgres',
+        'PASSWORD': conection.PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
